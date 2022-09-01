31f6af08fe5b83526eb8347907fc15de7d2435ed
nils <nils.nahooy@live.com>
2022-09-01 10:54:16 +0200

    Correct way of hiding statusbar.

--- app/src/main/java/com/nilsnahooy/quizapp/MainActivity.kt
+++ app/src/main/java/com/nilsnahooy/quizapp/MainActivity.kt
@@ -1,18 +1,11 @@
 package com.nilsnahooy.quizapp
 
-import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.View
-import androidx.annotation.RequiresApi
 
 class MainActivity : AppCompatActivity() {
-    @RequiresApi(Build.VERSION_CODES.R)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        window.decorView.windowInsetsController!!.hide(
-            android.view.WindowInsets.Type.statusBars()
-        )
     }
 }
