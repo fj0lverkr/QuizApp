ec634fe942dd42254c1be5575d8295597e3f71b7
Nils Nahooy <nils.nahooy@live.com>
2022-09-08 11:03:19 +0200

    Initial layout for final activity done.

---
+++ app/src/main/java/com/nilsnahooy/quizapp/ActivityQuizResult.kt
@@ -0,0 +1,11 @@
+package com.nilsnahooy.quizapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ActivityQuizResult : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_quiz_result)
+    }
+}
